c1f25ada9c2d464d78b0d13e34183be0f0101b0a
Emilse <emilsearias@gmail.com>
2021-11-18 18:10:54 -0300

    agregamos la logica del navbar para trabajar con rutas

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,17 +1,18 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">CRUD Cafe</Navbar.Brand>
+        <Link to='/' className='navbar-brand'>CRUD Cafe</Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#link">Inicio</Nav.Link>
-              <Nav.Link href="#link">Productos</Nav.Link>
-              <Nav.Link href="#link">Extra</Nav.Link>
+              <Link to='/' className='nav-link'>Inicio</Link>
+              <Link to='/productos' className='nav-link'>Productos</Link>
+              <Link to='/error404' className='nav-link'>Extra</Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
